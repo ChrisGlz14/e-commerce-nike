d826e0d54c8bfb09c4ad3b47d04fb00870d25ad7
Chrisglz14 <Christiang14@hotmail.com.ar>
2024-07-18 06:03:35 -0300

    features

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -24,27 +24,33 @@ export default function ProductPage({params}:Props) {
     }
 
     return (
-        <>
-    <main className="flex flex-col lg:flex-row m-4 lg:mt-9 lg:justify-center lg:m-auto gap-7 lg:w-[1000px]">
-        <div className="bg-red-400 w-full lg:w-28 mb-4 lg:mb-0">hola aca va un carrusel</div>
-        <div className="w-full lg:w-[500px] flex justify-center m-auto mb-4 lg:mb-0">
-            <Image className="h-auto w-full rounded-md" src={link + product?.images[0] ?? 'not found'} alt={product?.title ?? 'not found'} width={300} height={300}/>
-        </div>
+    <>
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:mt-9">
+                {/* Slider */}
+                {/* <div className="">hola aca va un carrusel de imagenes</div> */}
 
-        <div className="flex flex-col w-full lg:w-auto">
-            <h1 className={`text-2xl mt-5 lg:mt-0 lg:text-3xl ${titleFont.className}`}>{product?.title}</h1>
-            <p className="text-xl lg:text-2xl my-5">{product?.size[1]}</p>
-            <h2 className="text-lg lg:text-xl font-bold">${product?.price}</h2>
-            <div className="w-full lg:w-96">
-                <p className="mt-5 mb-5">{product?.description}</p>
-            </div>
-            {/* after of all we can add a size selector and effect button */}
-            <div className="flex mt-auto">
-                <button className="bg-white text-black rounded-full p-4 font-bold w-full">Agregar al carrito</button> 
+                <div className="m-2 md:col-span-2 md:w-[700px] mb-4 lg:mb-0 place-self-center">
+                    <Image className="h-auto w-full rounded-md" src={link + product?.images[0] ?? 'not found'} alt={product?.title ?? 'not found'} width={300} height={300}/>
+                </div>
+
+                {/* Details */}
+            <div className="grid col-span-1 px-5">
+                    {/* title */}
+                    <h1 className={`text-2xl lg:mt-0 lg:text-3xl antialiased ${titleFont.className}`}>{product?.title}</h1>
+                    {/* Size */}
+                    <p className="text-xl lg:text-2xl ">{product?.size.map((size, index)=> <span key={index}>{size}     </span>)}</p>
+                    {/* Price */}
+                    <h2 className="text-lg lg:text-xl font-bold">${product?.price}</h2>
+                    {/* Description */}
+                        <h3 className="text-lg font-bold">Descripcion</h3>
+                        <p className="text-sm">{product?.description}</p>
+                    {/* after of all we can add a size selector and effect button */}
+                    <div className="place-self-end w-full">
+                        <button className="bg-white text-black rounded-full p-4 font-bold w-full">Agregar al carrito</button> 
+                    </div>
             </div>
         </div>
-    </main>
-</>
+    </>
 
     );
 }
