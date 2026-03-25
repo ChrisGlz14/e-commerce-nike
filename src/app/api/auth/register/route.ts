adbc8ef75f65e26bc4f828a8884e8dffb2359a3f
chrisglz14 <chrisgd10s@gmail.com>
2026-03-25 06:11:04 -0300

    Cambio de route

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,12 +1,42 @@
 import { NextResponse } from "next/server";
 import { connectDB } from "@/lib/mongodb";
 import { User } from "@/models/user";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 export async function POST(req: Request) {
+  try {
+    const body = await req.json();
 
+    const { name, email, password } = body;
+
+    await connectDB();
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return NextResponse.json(
+        { message: "El usuario ya existe" },
+        { status: 400 },
+      );
+    }
+
+    // Hashear password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Creo usuario
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    return NextResponse.json(newUser);
+  } catch {
+
+    
   const body = await req.json();
-  console.log("BODY:", body);
 
   const { name, email, password } = body;
 
@@ -34,4 +64,5 @@ export async function POST(req: Request) {
   await newUser.save();
 
   return NextResponse.json(newUser);
+  }
 }
